a807f354001ae7498e7824496b61cc363da737ab
abelardo001 <abelardogs2000@gmail.com>
2025-12-15 13:51:28 +0000

    Atualizei banco para SQLite e adicionei imagens de mídia

--- emasever/settings.py
+++ emasever/settings.py
@@ -78,14 +78,11 @@ WSGI_APPLICATION = 'emasever.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME", "emasever"),
-        'USER': os.getenv("DB_USER", "postgres"),
-        'PASSWORD': os.getenv("DB_PASSWORD", "200019ab"),
-        'HOST': os.getenv("DB_HOST", "localhost"),
-        'PORT': os.getenv("DB_PORT", "5432"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # caminho do arquivo do banco
     }
 }
+
 
 
 # Password validation
